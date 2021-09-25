3cb167c9b28a914290878ed29d6c3214c2466f17
unknown <nazarmartyn2000@gmail.com>
2021-09-25 17:50:38 +0300

    Fixing static root.

--- config/settings.py
+++ config/settings.py
@@ -90,10 +90,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    STATIC_DIR
-]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
